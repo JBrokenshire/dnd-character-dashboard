b8cf8914042a1e93253ddfdb12043b382d946258
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-06 17:43:03 +0100

    Fixed responsiveness for header and hero component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,15 +4,15 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <header class="w-full padding-x">
-    <nav class="flex gap-8 items-center justify-center lg:justify-between">
+    <nav class="flex gap-2 sm:gap-8 items-center justify-center lg:justify-between">
       <RouterLink class="w-[15%] min-w-[75px] max-w-[150px]" to="/">
         <img src="@/assets/img/website-logo.png" alt="Website Logo" class="aspect-square" />
       </RouterLink>
 
-      <div class="flex-center gap-2 lg:gap-8 text-xl font-bold">
-        <RouterLink class="nav-link" to="/"> Home</RouterLink>
-        <RouterLink class="nav-link" to="/characters">Characters</RouterLink>
-        <RouterLink class="nav-link" to="/characters/add">Create Character</RouterLink>
+      <div class="flex-center sm:gap-2 lg:gap-8 text-xl font-bold">
+        <RouterLink class="nav-link" to="/">Home</RouterLink>
+        <RouterLink class="nav-link" to="/characters"> Characters</RouterLink>
+        <RouterLink class="nav-link" to="/characters/add">Create</RouterLink>
       </div>
     </nav>
   </header>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -8,7 +8,16 @@
         A horde of goblins, a family of trolls or a mighty red dragon — Your party is ready for
         anything
       </p>
+
+      <RouterLink to="/characters">
+        <div
+          class="w-fit rounded-full transition-200 mt-4 px-6 py-3 bg-primary border-2 border-white text-white font-bold text-xl hover:bg-white hover:border-primary hover:text-primary"
+        >
+          View All Character
+        </div>
+      </RouterLink>
     </div>
+
     <div class="hero__image-container">
       <div class="hero__image">
         <img src="@/assets/img/wizard.png" alt="Wizard Hero" class="object-contain" />
