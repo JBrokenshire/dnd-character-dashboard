eb1aaeac4719fcaf6229b2da333d6f936014400b
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-09 08:37:18 +0100

    Fixed the position and size of the images in the hero component.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -25,7 +25,7 @@
 
 <style scoped>
 .hero {
-  @apply flex xl:flex-row flex-col gap-5 relative z-0;
+  @apply flex xl:flex-row flex-col gap-5 relative z-0 mb-16;
 }
 
 .hero__title {
@@ -37,14 +37,14 @@
 }
 
 .hero__image-container {
-  @apply xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen;
+  @apply relative xl:flex-[1.5] justify-center items-center xl:justify-end xl:items-end w-full xl:h-screen flex;
 }
 
 .hero__image {
-  @apply relative max-w-[800px] xl:w-full w-[90%] xl:h-full h-[590px] z-0;
+  @apply flex justify-center items-start max-w-[800px] xl:w-full w-[50%] xl:h-full z-0;
 }
 
 .hero__image-overlay {
-  @apply absolute xl:top-0 md:bottom-0 bottom-32 xl:-right-1/4 bg-hero-bg bg-repeat-round -z-10 sm:w-[75%] w-[100%] xl:h-[80%] md:h-[540px] h-[480px] overflow-hidden;
+  @apply absolute xl:top-0 bottom-0 xl:left-0 left-auto bg-hero-bg bg-repeat-round -z-10 xl:w-[125%] w-[80%] xl:h-[80%] h-full overflow-hidden;
 }
 </style>
