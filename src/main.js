238791e029835eae1dc7e11db4c69a72ad0c6413
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-10 09:03:51 +0100

    Updated position for toast notifications

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import '@/assets/styles.css'
 import router from './router'
 import { createApp } from 'vue'
 import 'primeicons/primeicons.css'
-import Toast from 'vue-toastification'
+import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(Toast)
+app.use(Toast, {
+  position: POSITION.BOTTOM_CENTER,
+  timeout: 1500
+})
 
 app.mount('#app')
