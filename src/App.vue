66c1bad40a68e494351ef96e698149438ebc6003
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-10 09:19:33 +0100

    Fixed page scrolling left and right

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 </script>
 
-<template>
+<template class="overflow-x-hidden">
   <Header />
   <RouterView />
   <Footer />
