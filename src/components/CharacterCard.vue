66506f7fb65d144395b93c1717f38b5ef8951294
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-10 09:21:13 +0100

    Added hover effect for character card shadow

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -34,7 +34,9 @@ const deleteCharacter = async (characterID) => {
 </script>
 
 <template>
-  <div class="rounded-2xl bg-blue-50 transition-200 shadow-xl hover:scale-[1.05]">
+  <div
+    class="rounded-2xl bg-blue-50 transition-200 shadow-xl hover:scale-[1.05] hover:shadow-gray-400"
+  >
     <RouterLink :to="`/characters/${character.id}`">
       <div
         class="p-6 flex flex-grow gap-4 items-center hover:cursor-pointer relative overflow-hidden"
