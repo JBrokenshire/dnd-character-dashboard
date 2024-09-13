da042ae28c7b285d6ae9b93ca5810996724de074
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-13 09:13:14 +0100

    Prevented horizontal scrolling and changed target url to match updated api

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@ import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 </script>
 
-<template class="overflow-x-hidden">
+<template>
   <Header />
-  <RouterView />
+  <div class="overflow-x-hidden">
+    <RouterView />
+  </div>
   <Footer />
 </template>
 
