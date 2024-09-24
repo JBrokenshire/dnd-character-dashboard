87f98acb7e08b340b2e0c16f5db8395bb21178b3
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-24 08:55:36 +0100

    Removed class and race descriptions from character sheet.

--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -31,47 +31,5 @@ onMounted(async () => {
       Back To Character List
     </RouterLink>
     <CharacterSheetHeader :character="character" />
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
-      <div>
-        <RouterLink
-          :to="`/classes/${character.class_id}`"
-          class="w-fit mx-auto flex items-center gap-4 m-4 nav-link"
-        >
-          <h2 class="text-xl md:text-3xl font-bold text-center">
-            {{ character.class.name }}
-          </h2>
-          <i class="pi pi-chevron-right text-2xl" />
-        </RouterLink>
-        <div class="border border-gray-300 rounded-md px-4">
-          <p
-            class="py-2"
-            v-for="(section, index) in character.class.long_description.split('\n')"
-            :key="`character-class-description-section-${index}`"
-          >
-            {{ section }}
-          </p>
-        </div>
-      </div>
-      <div>
-        <RouterLink
-          :to="`/races/${character.race_id}`"
-          class="w-fit mx-auto flex items-center gap-4 m-4 nav-link"
-        >
-          <h2 class="text-xl md:text-3xl font-bold text-center">
-            {{ character.race.name }}
-          </h2>
-          <i class="pi pi-chevron-right text-2xl" />
-        </RouterLink>
-        <div class="border border-gray-300 rounded-md px-4">
-          <p
-            class="py-2"
-            v-for="(section, index) in character.race.long_description.split('\n')"
-            :key="`character-race-description-section-${index}`"
-          >
-            {{ section }}
-          </p>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
