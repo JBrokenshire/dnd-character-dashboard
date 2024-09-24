5d04d3b67bdef6b089b44def8b9f97a40099f401
JBrokenshire <jbrokenshire0306@gmail.com>
2024-09-24 17:25:25 +0100

    Added a small border to the bottom of the header

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -5,111 +5,121 @@ const isNavOpen = ref(false)
 </script>
 
 <template>
-  <header class="max-width padding-x">
-    <div class="flex-between py-2">
-      <RouterLink class="w-[15%] min-w-[75px] max-w-[150px]" to="/" @click="isNavOpen = false">
-        <img src="../../assets/img/website-logo.png" alt="Website Logo" class="aspect-square" />
-      </RouterLink>
-      <nav>
-        <!-- Small Screen Navbar -->
-        <section class="flex md:hidden">
-          <button class="space-y-2 transition hover:text-primary" @click="isNavOpen = !isNavOpen">
-            <span class="sr-only">Open Main Menu</span>
-            <svg
-              class="w-8 h-8"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-
-          <div :class="`transition-200 sidebar ${isNavOpen ? 'showMenuNav' : 'hideMenuNav'}`">
-            <div class="w-full flex-between px-4 py-2 mb-4">
-              <RouterLink
-                class="w-[15%] min-w-[75px] max-w-[150px] block sm:hidden"
-                to="/"
-                @click="isNavOpen = false"
+  <header class="w-full border-b border-gray-300 xl:mb-8">
+    <div class="max-width padding-x">
+      <div class="flex-between py-2">
+        <RouterLink class="w-[15%] min-w-[50px] max-w-[75px]" to="/" @click="isNavOpen = false">
+          <img src="../../assets/img/website-logo.png" alt="Website Logo" class="aspect-square" />
+        </RouterLink>
+        <nav>
+          <!-- Small Screen Navbar -->
+          <section class="flex md:hidden">
+            <button class="space-y-2 transition hover:text-primary" @click="isNavOpen = !isNavOpen">
+              <span class="sr-only">Open Main Menu</span>
+              <svg
+                class="w-8 h-8"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
               >
-                <img src="../../assets/img/website-logo.png" alt="Website Logo" class="aspect-square" />
-              </RouterLink>
-              <button class="w-full flex items-center justify-end" @click="isNavOpen = false">
-                <svg
-                  class="h-12 w-12 text-gray-600 transition-200 hover:text-primary"
-                  viewBox="0 0 24 24"
-                  fill="none"
+                <path
                   stroke="currentColor"
-                  stroke-width="1"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </button>
-            </div>
-            <ul
-              v-if="isNavOpen"
-              class="flex flex-col items-center sm:items-end justify-between gap-8 text-2xl px-8"
-            >
-              <li class="my-2">
-                <RouterLink class="nav-link px-2 md:px-4" to="/" @click="isNavOpen = false">
-                  Home
-                </RouterLink>
-              </li>
-              <li class="my-2">
+                  stroke-width="1"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+
+            <div :class="`transition-200 sidebar ${isNavOpen ? 'showMenuNav' : 'hideMenuNav'}`">
+              <div class="w-full flex-between px-4 py-2 mb-4">
                 <RouterLink
-                  class="nav-link px-2 md:px-4"
-                  to="/characters"
+                  class="w-[15%] min-w-[25px] max-w-[50px] block sm:hidden"
+                  to="/"
                   @click="isNavOpen = false"
                 >
-                  Characters
+                  <img
+                    src="../../assets/img/website-logo.png"
+                    alt="Website Logo"
+                    class="aspect-square"
+                  />
                 </RouterLink>
-              </li>
-              <li class="my-2">
-                <RouterLink class="nav-link px-2 md:px-4" to="/classes" @click="isNavOpen = false">
-                  Classes
-                </RouterLink>
-              </li>
-              <li class="my-2">
-                <RouterLink class="nav-link px-2 md:px-4" to="/races" @click="isNavOpen = false">
-                  Races
-                </RouterLink>
-              </li>
-            </ul>
-          </div>
-        </section>
+                <button class="w-full flex items-center justify-end" @click="isNavOpen = false">
+                  <svg
+                    class="h-12 w-12 text-gray-600 transition-200 hover:text-primary"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="1"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </button>
+              </div>
+              <ul
+                v-if="isNavOpen"
+                class="flex flex-col items-center sm:items-end justify-between gap-8 text-2xl px-8"
+              >
+                <li class="my-2">
+                  <RouterLink class="nav-link px-2 md:px-4" to="/" @click="isNavOpen = false">
+                    Home
+                  </RouterLink>
+                </li>
+                <li class="my-2">
+                  <RouterLink
+                    class="nav-link px-2 md:px-4"
+                    to="/characters"
+                    @click="isNavOpen = false"
+                  >
+                    Characters
+                  </RouterLink>
+                </li>
+                <li class="my-2">
+                  <RouterLink
+                    class="nav-link px-2 md:px-4"
+                    to="/classes"
+                    @click="isNavOpen = false"
+                  >
+                    Classes
+                  </RouterLink>
+                </li>
+                <li class="my-2">
+                  <RouterLink class="nav-link px-2 md:px-4" to="/races" @click="isNavOpen = false">
+                    Races
+                  </RouterLink>
+                </li>
+              </ul>
+            </div>
+          </section>
 
-        <!-- Large Screen Navbar -->
-        <ul class="hidden space-x-4 md:flex text-xl">
-          <li>
-            <RouterLink class="nav-link px-2" to="/" @click="isNavOpen = false">Home</RouterLink>
-          </li>
-          <li>
-            <RouterLink class="nav-link px-2" to="/characters" @click="isNavOpen = false"
-              >Characters</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink class="nav-link px-2" to="/classes" @click="isNavOpen = false"
-              >Classes</RouterLink
-            >
-          </li>
-          <li>
-            <RouterLink class="nav-link px-2" to="/races" @click="isNavOpen = false"
-              >Races</RouterLink
-            >
-          </li>
-        </ul>
-      </nav>
+          <!-- Large Screen Navbar -->
+          <ul class="hidden space-x-4 md:flex text-xl">
+            <li>
+              <RouterLink class="nav-link px-2" to="/" @click="isNavOpen = false">Home</RouterLink>
+            </li>
+            <li>
+              <RouterLink class="nav-link px-2" to="/characters" @click="isNavOpen = false"
+                >Characters</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink class="nav-link px-2" to="/classes" @click="isNavOpen = false"
+                >Classes</RouterLink
+              >
+            </li>
+            <li>
+              <RouterLink class="nav-link px-2" to="/races" @click="isNavOpen = false"
+                >Races</RouterLink
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 </template>
