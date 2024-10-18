eab459902f83a2ed3964003a8d4921dbe2fb167f
JBrokenshire <jbrokenshire0306@gmail.com>
2024-10-18 15:05:40 +0100

    Moved spell modifiers into a separate component for clarity

---
+++ src/components/character/sheet/subsections/primary/spells/SpellModifierHeader.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { characterProficiencyBonus } from '@/models/Character'
+
+const props = defineProps<{
+  className: string
+  spellcastingAbilityModifier: number
+  characterLevel: number
+}>()
+
+const spellcastingAttackModifier = computed(() => {
+  return props.spellcastingAbilityModifier + characterProficiencyBonus(props.characterLevel)
+})
+const spellSaveDC = computed(() => {
+  return 8 + spellcastingAttackModifier.value
+})
+</script>
+
+<template>
+  <div class="border-b mb-[10px] pb-[10px] flex w-full" :class="`border-${className}-faded`">
+    <div class="flex flex-1 gap-[16px] justify-center">
+      <!-- Spellcasting Ability Modifier -->
+      <div class="cs-subsection__spells-spellcasting-info">
+        <div class="flex">
+          <span class="text-[16px] text-white">
+            <span class="inline-flex items-center align-top">
+              <span class="text-cs-gray text-[.75rem] mr-[.063rem]">
+                {{ spellcastingAbilityModifier >= 0 ? '+' : '-' }}
+              </span>
+              <span>{{ Math.abs(spellcastingAbilityModifier) }}</span>
+            </span>
+          </span>
+        </div>
+        <div class="text-cs-gray uppercase text-[10px]">Modifier</div>
+      </div>
+
+      <!-- Spellcasting Attack Modifier -->
+      <div class="cs-subsection__spells-spellcasting-info">
+        <div class="flex">
+          <span class="text-[16px] text-white">
+            <span class="inline-flex items-center align-top">
+              <span class="text-cs-gray text-[.75rem] mr-[.063rem]">
+                {{ spellcastingAttackModifier >= 0 ? '+' : '-' }}
+              </span>
+              <span>{{ Math.abs(spellcastingAttackModifier) }}</span>
+            </span>
+          </span>
+        </div>
+        <div class="text-cs-gray uppercase text-[10px]">Spell Attack</div>
+      </div>
+
+      <!-- Spell Save DC -->
+      <div class="cs-subsection__spells-spellcasting-info">
+        <div class="flex">
+          <span class="text-[16px] text-white">
+            <span class="inline-flex items-center align-top">
+              <span>{{ spellSaveDC }}</span>
+            </span>
+          </span>
+        </div>
+        <div class="text-cs-gray uppercase text-[10px]">Save DC</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cs-subsection__spells-spellcasting-info {
+  @apply flex flex-col items-center font-bold tracking-tightest leading-[1];
+}
+</style>
--- src/components/character/sheet/subsections/primary/spells/SpellsContainer.vue
+++ src/components/character/sheet/subsections/primary/spells/SpellsContainer.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue'
-import { type Character, characterProficiencyBonus } from '@/models/Character'
+import { onMounted, ref } from 'vue'
+import { type Character } from '@/models/Character'
 import { modifierFromLevel } from '@/utils/utils'
+import SpellModifierHeader from '@/components/character/sheet/subsections/primary/spells/SpellModifierHeader.vue'
+import TitledSection from '@/components/character/sheet/subsections/primary/TitledSection.vue'
 
 const props = defineProps<{
   className: string
@@ -23,72 +25,22 @@ onMounted(() => {
 })
 
 const spellcastingAbilityModifier = ref(0)
-
-const spellcastingAttackModifier = computed(() => {
-  return spellcastingAbilityModifier.value + characterProficiencyBonus(props.character.level)
-})
-const spellSaveDC = computed(() => {
-  return 8 + spellcastingAttackModifier.value
-})
 </script>
 
 <template>
   <section class="flex flex-col w-full h-[600px] overflow-y-scroll pb-2 text-[14px] text-white">
     <h2 class="sr-only">Spells</h2>
 
-    <div class="border-b mb-[10px] pb-[10px] flex w-full" :class="`border-${className}-faded`">
-      <div
-        v-if="character.class.spellcasting_ability"
-        class="flex flex-1 gap-[16px] justify-center"
-      >
-        <!-- Spellcasting Ability Modifier -->
-        <div class="cs-subsection__spells-spellcasting-info">
-          <div class="flex">
-            <span class="text-[16px] text-white">
-              <span class="inline-flex items-center align-top">
-                <span class="text-cs-gray text-[.75rem] mr-[.063rem]">
-                  {{ spellcastingAbilityModifier >= 0 ? '+' : '-' }}
-                </span>
-                <span>{{ Math.abs(spellcastingAbilityModifier) }}</span>
-              </span>
-            </span>
-          </div>
-          <div class="text-cs-gray uppercase text-[10px]">Modifier</div>
-        </div>
+    <spell-modifier-header
+      v-if="character.class.spellcasting_ability"
+      :class-name="className"
+      :spellcasting-ability-modifier="spellcastingAbilityModifier"
+      :character-level="character.level"
+    />
 
-        <!-- Spellcasting Attack Modifier -->
-        <div class="cs-subsection__spells-spellcasting-info">
-          <div class="flex">
-            <span class="text-[16px] text-white">
-              <span class="inline-flex items-center align-top">
-                <span class="text-cs-gray text-[.75rem] mr-[.063rem]">
-                  {{ spellcastingAttackModifier >= 0 ? '+' : '-' }}
-                </span>
-                <span>{{ Math.abs(spellcastingAttackModifier) }}</span>
-              </span>
-            </span>
-          </div>
-          <div class="text-cs-gray uppercase text-[10px]">Spell Attack</div>
-        </div>
-
-        <!-- Spell Save DC -->
-        <div class="cs-subsection__spells-spellcasting-info">
-          <div class="flex">
-            <span class="text-[16px] text-white">
-              <span class="inline-flex items-center align-top">
-                <span>{{ spellSaveDC }}</span>
-              </span>
-            </span>
-          </div>
-          <div class="text-cs-gray uppercase text-[10px]">Save DC</div>
-        </div>
-      </div>
-    </div>
+    <titled-section :class-name="className">
+      <template #title>Cantrips</template>
+      <template #content></template>
+    </titled-section>
   </section>
 </template>
-
-<style scoped>
-.cs-subsection__spells-spellcasting-info {
-  @apply flex flex-col items-center font-bold tracking-tightest leading-[1];
-}
-</style>
